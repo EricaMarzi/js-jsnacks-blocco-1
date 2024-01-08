a97d2cafd91fc89fff95964eb9f92409590d8385
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-08 12:23:12 +0100

    ciclo while

--- array-bonus/script.js
+++ array-bonus/script.js
@@ -3,7 +3,7 @@ Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 */
 
-const fridge = ['banana', 'mela', 'pera']
+/*const fridge = ['banana', 'mela', 'pera']
 
 const table = ['ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
 
@@ -19,6 +19,27 @@ for (let i = 0 ; i < table.length && fridge.length !== table.length; i++) {
     console.log("_______________________________________________")
     console.table(fridge)
     console.table(table)
+}*/
+
+
+/* 
+1. Individuo quale array è più lungo e quale è più corto
+2. Calcolo la differenza 
+3. Genero dei numeri casuali TANTI QUANTI ne mancano al più corto per arrivare al più lungo
+*/
+
+const array1 = [5, 7];
+const array2 = [3, 54, 6, 3, 23];
+
+const shorterArray = array1.length > array2.length ? array2 : array1
+const longerArray = array1.length > array2.length ? array1 : array2
+
+while (array1.length !== array2.length) {
+    const randomNumber = Math.floor (Math.random () * 100) +1
+    shorterArray. push(randomNumber)
 }
+
+console.table (shorterArray)
+console.table (longerArray)
 
 
